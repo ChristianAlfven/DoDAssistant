4f4150b723f9fa4d5b003c1825b23cda69a2bcf4
Markus Lindgren <markus.iraye.lindgren@google.com>
2019-05-02 15:08:04 +0200

    DB_Connector

---
+++ src/DB_Connector.java
@@ -0,0 +1,28 @@
+/*
+Chess Game Server: the user retrieves the other player’s latest move and/or submits his own latest move.
+The move can be represented by a string in a table.
+Each player move results in a new row in a table.
+The table has two columns: Player ID and Move.
+The string “Qa1”, means: Queen moves to A1.
+*/
+
+
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class DB_Connector {
+
+    String url = "jdbc:mysql://localhost:3306/dod_db?user=root&password=root&serverTimezone=UTC";
+
+    Statement statement;
+
+    public DB_Connector() {
+        try {
+            Connection c = DriverManager.getConnection(url);
+            statement = c.createStatement();
+        } catch (SQLException ex) {
+            System.out.println("the connection fails");
+            ex.printStackTrace();
+        }
+    }
+}

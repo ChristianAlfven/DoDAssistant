1ac609852d00bd98e2896aad77cf845110f0b0fa
Markus Lindgren <markus.iraye.lindgren@google.com>
2019-05-06 16:08:42 +0200

    GUICombat

---
+++ src/Combat/CombatTest.java
@@ -0,0 +1,25 @@
+package Combat;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class CombatTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Pane root = FXMLLoader.load(getClass().getResource("GUIInitiative.fxml"));
+        primaryStage.setTitle("Initiative");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
